387e9debc171b091a64d438e7f428fc28c34f18d
Jose Daniel Acuna <jodaniel1409@gmail.com>
2021-06-10 18:14:45 -0600

    Init API

---
+++ Controllers/OffensiveController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocAnalyzerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OffensiveController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
---
+++ Controllers/SentimentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocAnalyzerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SentimentController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
