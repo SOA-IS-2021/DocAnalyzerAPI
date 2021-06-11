b27ce72bf14d0e38840f7c51f26710dfb02930c3
Jose Daniel Acuna <jodaniel1409@gmail.com>
2021-06-11 17:21:40 -0600

    Connected to broker

---
+++ Controllers/BrokerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace DocAnalyzerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrokerController : ControllerBase
+    {
+        [HttpGet]
+        public void Get(string name)
+        {
+            var body = Encoding.UTF8.GetBytes(name);
+            Program.Channel.ExchangeDeclare(exchange: "Document-Analyzer",
+                type: ExchangeType.Fanout);
+            Program.Channel.BasicPublish(exchange: "Document-Analyzer",
+                routingKey: "",
+                basicProperties: null,
+                body: body);
+            Console.WriteLine(" Message sent: " + name);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 
 namespace DocAnalyzerAPI
 {
     public class Program
     {
+        public static IModel Channel;
+        
         public static void Main(string[] args)
         {
+            var factory = new ConnectionFactory{ Uri = new Uri("amqp://guest:guest@rabbitmq-server:5672/")  };
+            var connection = factory.CreateConnection();
+            Channel = connection.CreateModel();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
